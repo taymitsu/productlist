64949ce3e52d05108d11b5af7018904919e09c21
taymitsu <tmitsumori96@gmail.com>
2023-01-11 23:12:16 -0800

    buttons and product info displaying

--- src/App.js
+++ src/App.js
@@ -2,16 +2,43 @@ import './App.css';
 import data, { allCategories, reduceCategories, categoryWithCount, nameAndCount } from './data' //imports data.js file, no extension 
 // NON DEFAULT EXPORTS { } //
 
-console.log(data)
-console.log(allCategories)
-console.log(reduceCategories)
-console.log(categoryWithCount) //EX: {Movies: 6}
-console.log(nameAndCount) //EX: {Name: Movie, count: 6}
+//console.log(data)
+//console.log(allCategories)
+//console.log(reduceCategories)
+//console.log(categoryWithCount) //EX: {Movies: 6}
+//console.log(nameAndCount) //EX: {Name: Movie, count: 6}
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Product Shop</h1>
+      <p>Product Count: { data.length }</p>
+      <p>Category Count: { reduceCategories.length }</p>
+      
+      <div>
+        {nameAndCount.map(obj => {
+          return (
+            <button>
+              {obj.name}
+              <span> {obj.count} </span>
+            </button>
+          )
+        })}
+      </div>
+
+      <div>
+        {data.map(obj => {
+          return (
+            <div>
+              <h2>{obj.name}</h2>
+              <p>{obj.description}</p>
+              <p>{obj.price}</p>
+              <small>Category: {obj.category} Rating: {obj.rating}</small>
+            </div>
+          )
+        })}
+      </div>
+
     </div>
   );
 }
